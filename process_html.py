fa82fa79f7522d6670fc2c61080372b700a54831
Matt Hall <matt@agilescientific.com>
2022-04-07 11:42:23 -0300

    lol, call func

--- process_html.py
+++ process_html.py
@@ -41,6 +41,7 @@ def main(path):
             html = f.read()
 
             new_html = simplify_credits(html)
+            new_html = add_analytics(html)
 
             f.seek(0)
             f.write(new_html)
